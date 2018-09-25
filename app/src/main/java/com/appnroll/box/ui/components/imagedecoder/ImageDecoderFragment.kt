da09ad38b4363d41245fc180788d04fba67a1489
mdabrowski <m.dabrowski@appnroll.com>
2018-09-25 17:24:02 +0200

    Update gradle to 4.6, refactor ImageDecoderFragment - add loading images using BitmapFactory

--- app/src/main/java/com/appnroll/box/ui/components/imagedecoder/ImageDecoderFragment.kt
+++ app/src/main/java/com/appnroll/box/ui/components/imagedecoder/ImageDecoderFragment.kt
@@ -9,53 +9,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import com.appnroll.box.R
-import com.appnroll.box.utils.getFromAssets
 import kotlinx.android.synthetic.main.fragment_image_decoder.*
 import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.GlobalScope
 import kotlinx.coroutines.experimental.android.Main
 import kotlinx.coroutines.experimental.launch
+import java.io.IOException
 
 
 class ImageDecoderFragment : Fragment() {
 
-    private val emptyListener = ImageDecoder.OnHeaderDecodedListener { _, _, _ ->
-        // do nothing
-    }
-
-    private val resizeListener = ImageDecoder.OnHeaderDecodedListener { decoder, info, _ ->
+    private val emptyListener = ImageDecoder.OnHeaderDecodedListener { decoder, _, _ ->
         decoder.setOnPartialImageListener { true }
-        decoder.setTargetSize(info.size.width / 2, info.size.height / 2)
     }
 
-    private val cropListener = ImageDecoder.OnHeaderDecodedListener { decoder, info, _ ->
-        decoder.setOnPartialImageListener { true }
-        decoder.crop = getCropRect(info.size.width / 2, info.size.height / 2)
-    }
-
-    private val postProcessListener = ImageDecoder.OnHeaderDecodedListener { decoder, _, _ ->
-        decoder.setOnPartialImageListener { true }
-
-        // This will add round corners to the image/gif
-        val path = Path().apply {
-            fillType = Path.FillType.INVERSE_EVEN_ODD
-        }
-        val paint = Paint().apply {
-            isAntiAlias = true
-            color = Color.TRANSPARENT
-            xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC)
-        }
-        decoder.setPostProcessor { canvas ->
-            val width = canvas.width.toFloat()
-            val height = canvas.height.toFloat()
-            path.addRoundRect(0f, 0f, width, height, 40f, 40f, Path.Direction.CW)
-            canvas.drawPath(path, paint)
-            PixelFormat.TRANSLUCENT
-        }
-    }
-
-    private var assetFile: AssetFile = AssetFile.CatJpg
-    private var decodeListener: ImageDecoder.OnHeaderDecodedListener = emptyListener
+    private var assetFileName: String = ASSET_FILE_CAT_JPG
+    private var decodedListener: ImageDecoder.OnHeaderDecodedListener = emptyListener
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_image_decoder, container, false)
@@ -73,59 +42,116 @@ class ImageDecoderFragment : Fragment() {
         croppedButton.setOnCheckedChangeListener(onCheckedChangeListener)
         postProcessedButton.setOnCheckedChangeListener(onCheckedChangeListener)
 
-        updateImage()
+        catJpgCheckbox.setOnCheckedChangeListener { _, isChecked ->
+            originalButton.isEnabled = !isChecked
+            resizedButton.isEnabled = !isChecked
+            croppedButton.isEnabled = !isChecked
+            postProcessedButton.isEnabled = !isChecked
+
+            updateImage(assetFileName, decodedListener)
+        }
+
+        updateImage(assetFileName, decodedListener)
     }
 
     private val onCheckedChangeListener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
         if (isChecked) {
             when (buttonView) {
-                catJpgButton -> assetFile = AssetFile.CatJpg
-                catWebpButton -> assetFile = AssetFile.CatWebp
-                catGifButton -> assetFile = AssetFile.CatGif
-                originalButton -> decodeListener = emptyListener
-                resizedButton -> decodeListener = resizeListener
-                croppedButton -> decodeListener = cropListener
-                postProcessedButton -> decodeListener = postProcessListener
+                catJpgButton -> assetFileName = ASSET_FILE_CAT_JPG
+                catWebpButton -> assetFileName = ASSET_FILE_CAT_WEBP
+                catGifButton -> assetFileName = ASSET_FILE_CAT_GIF
+                originalButton -> decodedListener = emptyListener
+                resizedButton -> decodedListener = resizeListener
+                croppedButton -> decodedListener = cropListener
+                postProcessedButton -> decodedListener = postProcessListener
             }
-            updateImage()
+            updateImage(assetFileName, decodedListener)
         }
     }
 
-    private fun updateImage() {
+    private fun updateImage(assetFileName: String, decodedListener: ImageDecoder.OnHeaderDecodedListener) {
+        if (catJpgCheckbox.isChecked) {
+            updateImageWithBitmapFactory(assetFileName)
+        } else {
+            updateImageWithImageDecoder(assetFileName, decodedListener)
+        }
+    }
+
+    private fun updateImageWithBitmapFactory(assetFileName: String) {
+        val context = requireContext()
         GlobalScope.launch(Dispatchers.Default) {
-            requireContext().getFromAssets(assetFile.name)
-                    .let { file -> ImageDecoder.createSource(file) }
-                    .let { source ->ImageDecoder.decodeDrawable(source, decodeListener) }
-                    .let { drawable ->
-                        GlobalScope.launch(Dispatchers.Main) {
-                            if (drawable is AnimatedImageDrawable) {
-                                drawable.start()
-                            }
-                            imageView.setImageDrawable(drawable)
-                        }
-                    }
+            val bitmap = try {
+                BitmapFactory.decodeStream(context.assets.open(assetFileName))
+            } catch (e: IOException) {
+                null
+            }
+            GlobalScope.launch(Dispatchers.Main) {
+                bitmap?.let {
+                    imageView.setImageBitmap(it)
+                }
+            }
         }
     }
 
-    private fun getCropRect(centerX: Int, centerY: Int): Rect {
+    private fun updateImageWithImageDecoder(assetFileName: String, decodedListener: ImageDecoder.OnHeaderDecodedListener) {
+        val context = requireContext()
+        GlobalScope.launch(Dispatchers.Default) {
+            val source = ImageDecoder.createSource(context.assets, assetFileName)
+            val drawable = ImageDecoder.decodeDrawable(source, decodedListener)
+            GlobalScope.launch(Dispatchers.Main) {
+                imageView.setImageDrawable(drawable)
+                if (drawable is AnimatedImageDrawable) {
+                    drawable.start()
+                }
+            }
+        }
+    }
+
+    private val resizeListener = ImageDecoder.OnHeaderDecodedListener { decoder, info, _ ->
+        decoder.setOnPartialImageListener { true }
+        decoder.setTargetSize(info.size.width / 2, info.size.height / 2)
+    }
+
+    private val cropListener = ImageDecoder.OnHeaderDecodedListener { decoder, info, _ ->
+        decoder.setOnPartialImageListener { true }
         val cropSize = 100
-        return Rect(
+        val centerX = info.size.width / 2
+        val centerY = info.size.height / 2
+        decoder.crop = Rect(
                 centerX - cropSize,
                 centerY - cropSize,
                 centerX + cropSize,
                 centerY + cropSize)
     }
 
+    private val postProcessListener = ImageDecoder.OnHeaderDecodedListener { decoder, _, _ ->
+        decoder.setOnPartialImageListener { true }
+
+        val path = Path().apply {
+            fillType = Path.FillType.INVERSE_EVEN_ODD
+        }
+        val paint = Paint().apply {
+            isAntiAlias = true
+            color = Color.TRANSPARENT
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC)
+        }
+        decoder.setPostProcessor { canvas ->
+            val width = canvas.width.toFloat()
+            val height = canvas.height.toFloat()
+            path.addRoundRect(0f, 0f, width, height, 40f, 40f, Path.Direction.CW)
+            canvas.drawPath(path, paint)
+            PixelFormat.TRANSLUCENT
+        }
+    }
+
     companion object {
+
+        const val ASSET_FILE_CAT_JPG = "cat.jpg"
+        const val ASSET_FILE_CAT_GIF = "cat.gif"
+        const val ASSET_FILE_CAT_WEBP = "cat.webp"
 
         fun getInstance(): Fragment {
             return ImageDecoderFragment()
         }
-    }
-
-    sealed class AssetFile(val name: String) {
-        object CatJpg: AssetFile("cat.jpg")
-        object CatGif: AssetFile("cat.gif")
-        object CatWebp: AssetFile("cat.webp")
     }
 }
